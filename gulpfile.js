fda6b6d3f1ca5969dcc63ec70efedcdecacd716a
Andrew Pohl <andrewpohl@Andrews-MacBook-Pro.local>
2016-09-11 12:16:04 -0700

    Initial Commit, adding bulk of the files
    * Adding the bulk of the files

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require("gulp-rename");
+var cdnizer = require("gulp-cdnizer");
+var browserSync = require('browser-sync').create();
+
+gulp.task('styles', function () {
+    return gulp.src('./app/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('style', function () {
+    return gulp.src('./app/scss/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./app/css'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('js', function() {
+    return gulp.src('./app/js/main.js')
+        .pipe(gulp.dest('./js'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('audio', function() {
+    return gulp.src('./app/audio/**/*.mp3')
+        .pipe(gulp.dest('./audio'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('artists', function() {
+    return gulp.src('./app/artists/*.html')
+        .pipe(gulp.dest('./artists'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('home', function() {
+    return gulp.src('./app/*.html')
+        .pipe(gulp.dest('./'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('videos', function() {
+    return gulp.src('./app/videos/eh5v.files/**/*.+(jpg|m4v|webm|js|png)')
+        .pipe(gulp.dest('./videos/eh5v.files'))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: './app/'
+        }
+    });
+
+    gulp.watch('./app/main.js', ['js']);
+    gulp.watch('./app/scss/*.scss', ['styles']);
+    gulp.watch('./**/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'style', 'js', 'audio', 'videos', 'artists', 'home', 'serve']);
---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('#header').load('_header.html');
+$('.artist-header').load("../_header.html");
+$('#footer').load('../_footer.html');
+
+$("img").on("click", function() {
+  var sr = $(this).attr('src'); 
+  $('#imagepreview').attr('src', sr);
+  $('#imagemodal').modal('show');
+});
